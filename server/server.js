0ff652a8f7fe60f4d9e2db06c8d4526af00239cc
GFG Developer <dev@gfg.org>
2026-01-28 00:57:36 +0700

    Fix: Final CORS stabilization - vercel.json headers + server.js cleanup

--- server/server.js
+++ server/server.js
@@ -9,17 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
+app.use(cors());
 app.use(express.json());
 
 // Basic Route
